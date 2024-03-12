7d8034bff4ce3538a8496d263fb27ad68b277a6a
Gleriston <xpanium@gmail.com>
2024-03-11 23:48:16 -0300

    add auth login page in the project

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,11 @@
+import LoginForm from '@/components/auth/login-form'
 import React from 'react'
 
 type Props = {}
 
 const LoginPage = (props: Props) => {
   return (
-    <div>Login Page</div>
+    <LoginForm />
   )
 }
 
