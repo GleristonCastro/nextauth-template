b909874738ff68b967be9bd520ca2385471e6edf
Gleriston <xpanium@gmail.com>
2024-03-16 20:10:27 -0300

    update: add send verification mail

--- actions/register.ts
+++ actions/register.ts
@@ -4,9 +4,10 @@ import * as z from "zod";
 import bcrypt from "bcryptjs";
 
 import { db } from "@/lib/db";
-
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -32,5 +33,12 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     }
   });
 
-  return { success: "User created!" };
+  const verificationToken = await generateVerificationToken(email);
+  
+  await sendVerificationEmail(
+    verificationToken.email,
+    verificationToken.token
+  );
+
+  return { success: "Confirmation email sent!" };
 };
