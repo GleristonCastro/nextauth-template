3d0b5fc3bdf3b18560a56fc002d94b0ec0015f8d
Gleriston <xpanium@gmail.com>
2024-03-16 20:08:56 -0300

    add new features: events, callbacks and adapters

--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth";
+import { UserRole } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
-import { db } from "./lib/db";
-
+import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
 
 export const {
   handlers: { GET, POST },
@@ -11,6 +12,58 @@ export const {
   signIn,
   signOut,
 } = NextAuth ( {
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update ({
+        where: {id: user.id},
+        data: { emailVerified: new Date()}
+      });
+    },
+  },
+  callbacks: {
+    async signIn({user, account}){
+      if (account?.provider !== "credentials") return true;
+
+      if (!user || !user.id) {
+        return false;
+      }
+
+      const existingUser = await getUserById(user.id);
+      
+      // Prevent sign in without email verification
+      if (!existingUser?.emailVerified) return false
+
+      // TODO: Add 2FA check
+
+      return true
+    },
+    async session({token, session}){
+      if(token.sub && session.user){
+        session.user.id = token.sub;
+      };
+
+      if(token.role && session.user){
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({token}){
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: {strategy: "jwt"},
   ...authConfig,
