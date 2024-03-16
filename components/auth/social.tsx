b4798bc8c0f28358ba75a72d68a31bac38dca94f
Gleriston <xpanium@gmail.com>
2024-03-16 20:12:29 -0300

    update and include routs default login

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,20 +1,27 @@
 "use client"
 
-import React from "react";
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa"
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 type Props = {};
 
 function Social({}: Props) {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5" />
       </Button>
@@ -22,7 +29,7 @@ function Social({}: Props) {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="h-5 w-5" />
       </Button>
