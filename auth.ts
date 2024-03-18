604cfedafa4aa19c5f3a5349d8681350450c2869
Gleriston <xpanium@gmail.com>
2024-03-17 22:12:27 -0300

    add 2fa verification with email and token

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 import { getUserById } from "@/data/user";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
 
 export const {
   handlers: { GET, POST },
@@ -26,6 +27,7 @@ export const {
   },
   callbacks: {
     async signIn({user, account}){
+      // Allow OAuth without email verification
       if (account?.provider !== "credentials") return true;
 
       if (!user || !user.id) {
@@ -35,9 +37,20 @@ export const {
       const existingUser = await getUserById(user.id);
       
       // Prevent sign in without email verification
-      if (!existingUser?.emailVerified) return false
+      if (!existingUser?.emailVerified) return false;
 
-      // TODO: Add 2FA check
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
+
+        if (!twoFactorConfirmation) return false
+
+        //Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: {
+            id: twoFactorConfirmation.id
+          }
+        })
+      }
 
       return true
     },
