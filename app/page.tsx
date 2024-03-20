49b430b5b5536d8929eb9cdf4d96bf993419f01d
Gleriston <xpanium@gmail.com>
2024-03-20 19:18:23 -0300

    update names in main page

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,13 @@ export default function Home() {
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <div className="space-y-6 text-center">
         <h1 className={cn(`text-6xl font-semibold text-white drop-shadow-md flex justify-center`, font.className)}>
-        <FcLock /> Auth
+        <FcLock /> Auth Template
         </h1>
         <p className="text-white text-lg">
-          A simple autentication service
+          A advanced autentication service
         </p>
         <div>
-          <LoginButton>
+          <LoginButton mode="modal" asChild>
             <Button variant="secondary" size="lg" >
               Sign in
             </Button>
